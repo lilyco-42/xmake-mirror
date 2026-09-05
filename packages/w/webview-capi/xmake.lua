92bdc5f3fb1dd5ab80e27d1186e9acc8da3457f6
周浩宇 <liuqingffffff@gmail.com>
2026-09-06 00:00:01 +0800

    fix: webview-capi 1.1.0 包定义 (v-tag URL + sha256 + lib/ 路径)

--- packages/w/webview-capi/xmake.lua
+++ packages/w/webview-capi/xmake.lua
@@ -3,21 +3,19 @@ package("webview-capi")
     set_description("C API for WebView2 - callable from any language (Python/Go/Rust/Node.js)")
     set_license("MIT")
 
-    set_urls("https://github.com/lilyco-42/webview-capi/archive/refs/tags/$(version).tar.gz",
+    set_urls("https://github.com/lilyco-42/webview-capi/archive/refs/tags/v$(version).tar.gz",
              "https://github.com/lilyco-42/webview-capi.git")
 
-    add_versions("1.0.0", "main")
-    add_versions("0.5.0", "main")
+    add_versions("1.1.0", "11420ea13763ebc82cbc21866e2107884d097fbd4d188c5dca3be506797645d1")
 
     add_syslinks("user32", "shell32", "ole32", "oleaut32", "shlwapi", "version")
-    add_includedirs(".")
 
     on_install(function (package)
-        -- 单头文件 + DLL 模式
-        os.cp("webview.h", package:installdir("include"))
+        -- lib/ 下: webview.h (单头文件) + webview.dll/.lib (预编译)
+        os.cp("lib/webview.h", package:installdir("include"))
         if package:is_plat("windows") then
-            os.cp("webview.dll", package:installdir("bin"))
-            os.cp("webview.lib", package:installdir("lib"))
+            os.cp("lib/webview.dll", package:installdir("bin"))
+            os.cp("lib/webview.lib", package:installdir("lib"))
         end
     end)
 
